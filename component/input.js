d868a452cfe4b880e55bd797c909643af0ca15fc
Marstin <417221906@qq.com>
2018-05-06 22:31:47 +0800

    搭建框架

---
+++ component/input.js
@@ -0,0 +1,10 @@
+require(['jquery'],function () {
+    $.fn.input = (options) => {
+        let defaults = {
+            name:'',
+            maxHeight:'',
+            minHeight:'',
+
+        };
+    }
+});
